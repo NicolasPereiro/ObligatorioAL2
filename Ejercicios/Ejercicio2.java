f60d5914856b7f78c10d2589803611b5df43a5d1
Andres <andrescarsin@gmail.com>
2023-09-14 19:06:14 -0300

    Teoricamente terminado

--- Ejercicios/Ejercicio2.java
+++ Ejercicios/Ejercicio2.java
@@ -3,6 +3,7 @@ package Ejercicios;
 import java.util.Scanner;
 import objetos.Genero;
 import objetos.Pelicula;
+import tads.colaPrioridad.MaxHeap;
 
 public class Ejercicio2 {
     public Ejercicio2() {
@@ -23,24 +24,61 @@ public class Ejercicio2 {
             String[] datosFilm = in.nextLine().split(" ");
             Pelicula newPelicula = new Pelicula(Integer.parseInt(datosFilm[0]));
             for (int i = 0; i <= largoGenros && !encontrado; i++) {
-                if ((arrGeneros[i].nombre).compareToIgnoreCase(datosFilm[1])) {
+                if ((arrGeneros[i].nombre).compareToIgnoreCase(datosFilm[1]) == 0) {
                     arrGeneros[i].peliculas.push(newPelicula, 0);
                     encontrado = true;
                 }
             }
             cantPeliculas--;
         }
-        int calificaciones=in.nextInt();
-        while(calificaciones!=0){
+        int calificaciones = in.nextInt();
+        while (calificaciones != 0) {
             String[] reseñaFilm = in.nextLine().split(" ");
-            
+            int id = Integer.parseInt(reseñaFilm[0]);
+            MaxHeap<Pelicula, Integer> heapAux = new MaxHeap<Pelicula, Integer>(100);
+            boolean cambio = false;
+            for (int i = 0; i < arrGeneros.length && !cambio; i++) {
+                Pelicula film = arrGeneros[i].peliculas.pop();
+                if (film.id == id) {
+                    film.calificaciones += Integer.parseInt(reseñaFilm[1]);
+                    film.nroReseñas++;
+                    arrGeneros[i].peliculas.push(film, film.calificaciones);
+                    while (heapAux.size() != 0) { // warning
+                        Pelicula peliculaAux = heapAux.pop();
+                        arrGeneros[i].peliculas.push(peliculaAux, peliculaAux.calificaciones);
+                    }
+                    cambio = true;
+                } else { // warning
+                    heapAux.push(film, film.calificaciones);
+                    if (arrGeneros[i].peliculas.size() == 0) {
+                        arrGeneros[i].peliculas = heapAux;
+                        heapAux = new MaxHeap<Pelicula, Integer>(100);
+                    }
+                }
+            }
         }
-
-        THC<String, Plato> t = new THC<String, Plato>(hash, largo);
-        for (int i = 0; i < largo; i++) {
-            String entrada = in.nextLine();
-
+        Pelicula[] peliculasMejorCalificadas = new Pelicula[largoGenros + 1];
+        for (int j = 0; j < peliculasMejorCalificadas.length; j++) {
+            peliculasMejorCalificadas[j] = null;
+        }
+        for (int i = 0; i < arrGeneros.length; i++) {
+            while (arrGeneros[i].peliculas.size() != 0) {
+                Pelicula film = arrGeneros[i].peliculas.pop();
+                int ReseñaProm = film.calificaciones / film.nroReseñas;
+                film.calificaciones = ReseñaProm;
+                if (peliculasMejorCalificadas[i] == null
+                        || peliculasMejorCalificadas[i].calificaciones < film.calificaciones) {
+                    peliculasMejorCalificadas[i] = film;
+                } else {
+                    if (peliculasMejorCalificadas[i].calificaciones == film.calificaciones
+                            && peliculasMejorCalificadas[i].id < film.id) {
+                        peliculasMejorCalificadas[i] = film;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < peliculasMejorCalificadas.length; i++) {
+            System.out.println(peliculasMejorCalificadas[i]);
         }
     }
-
 }
