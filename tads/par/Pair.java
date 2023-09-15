2f07193ec69a0ce9ceecd7bd7c0ec518ed95b9d9
NicoPereiro <NP284068@fi365.ort.edu.uy>
2023-09-15 08:02:28 -0300

    commit antes de merge

--- tads/par/Pair.java
+++ tads/par/Pair.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:tads/par/Pair.java
 package tads.par;
 
 public class Pair<T1, T2> {
@@ -17,23 +16,3 @@ public class Pair<T1, T2> {
       this.snd = snd;
     }
 }
-=======
-package tads.table;
-
-public class Pair<T1, T2> {
-    public T1 fst;
-    public T2 snd;
-  
-    public Pair() {
-    }
-  
-    public Pair(T1 fst) {
-      this.fst = fst;
-    }
-  
-    public Pair(T1 fst, T2 snd) {
-      this.fst = fst;
-      this.snd = snd;
-    }
-}
->>>>>>> ejercicio2:tads/table/Pair.java
