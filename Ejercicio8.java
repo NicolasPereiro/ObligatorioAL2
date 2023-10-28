7a0d8812dd265571d11306119d79ffbf9ce5c498
NicoPereiro <NP284068@fi365.ort.edu.uy>
2023-10-28 20:55:20 -0300

    Terminado ej8

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -14,39 +14,40 @@ public class Ejercicio8 {
         }
         in.close();
         encontrarPicos(datos, 0, largo - 1);
-        boolean print=false;
+        boolean print = false;
         for (int i = 0; i < datos.length; i++) {
-            if(((Pico)datos[i]).esPico==true){
-                System.out.println(((Pico)datos[i]).valor);
-                print=true;
+            if (((Pico) datos[i]).esPico == true) {
+                System.out.println(((Pico) datos[i]).valor);
+                print = true;
             }
         }
-        if(!print){
+        if (!print) {
             System.out.println("No se encontraron picos");
         }
     }
 
     private static void encontrarPicos(Object[] arr, int ini, int fin) {
-        if(ini-fin <=1){//en el 4 queda ini=2 overflow, cuando hay iguales seguidos boom
-            if(ini == fin){
-                ((Pico)arr[ini]).esPico = true;
-                return;//yo lo hice
-            } else if (((Pico) arr[ini]).valor > ((Pico)arr[fin]).valor){
-            } else if (((Pico) arr[ini]).valor < ((Pico)arr[fin]).valor){
-                ((Pico)arr[fin]).esPico = true;
+        if (fin - ini <= 1) {// en el 4 queda ini=2 overflow, cuando hay iguales seguidos boom
+            if (ini == fin) {
+                ((Pico) arr[ini]).esPico = true;
+            } else if (((Pico) arr[ini]).valor > ((Pico) arr[fin]).valor) {
+                ((Pico) arr[ini]).esPico = true;
+            } else if (((Pico) arr[ini]).valor < ((Pico) arr[fin]).valor) {
+                ((Pico) arr[fin]).esPico = true;
             }
+            return;
         }
-        encontrarPicos(arr, ini, fin/2);
-        encontrarPicos(arr, fin/2+1, fin);
-        if(((Pico)arr[fin/2]).esPico && ((Pico)arr[fin/2+1]).esPico){
-            if(((Pico) arr[fin/2]).valor > ((Pico)arr[fin/2+1]).valor){
-                 ((Pico)arr[fin/2+1]).esPico = false;
-            } else if (((Pico) arr[fin/2]).valor < ((Pico)arr[fin/2+1]).valor){
-                ((Pico)arr[fin/2]).esPico = false;
-            } else {
-                ((Pico)arr[fin/2]).esPico = false;
-                ((Pico)arr[fin/2+1]).esPico = false;
-            }
+        int mitad = (fin - ini) / 2;
+        mitad = ini + mitad;
+        encontrarPicos(arr, ini, mitad);
+        encontrarPicos(arr, mitad + 1, fin);
+        if (((Pico) arr[mitad]).valor > ((Pico) arr[mitad + 1]).valor) {
+            ((Pico) arr[mitad + 1]).esPico = false;
+        } else if (((Pico) arr[mitad]).valor < ((Pico) arr[mitad + 1]).valor) {
+            ((Pico) arr[mitad]).esPico = false;
+        } else {
+            ((Pico) arr[mitad]).esPico = false;
+            ((Pico) arr[mitad + 1]).esPico = false;
         }
     }
 }
