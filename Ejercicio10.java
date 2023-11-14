365ae4025ace404b0be7babc83d1680b690f2cd8
NicoPereiro <n.pereiro0@gmail.com>
2023-11-14 20:42:25 -0300

    Avance en Ejercicio10

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -15,50 +15,87 @@ public class Ejercicio10 {
                 mat[i][j] = Integer.parseInt(fila[j]);
             }
         }
+        int k = largo / 3 * ancho / 3;
         for (int i = 0; i < largo; i++) {
             for (int j = 0; j < ancho; j++) {
                 if (mat[i][j] == 0) {
-                    if (sudokuBT(mat, i, j)) {
-                        System.out.print(mat[i][j] + " ");
-                    }
+                    sudokuBT(mat, i, j, k);
                 }
             }
             System.out.println();
         }
     }
+    
 
-    private static boolean sudokuBT(int[][] mat, int i, int j) {
-        return false;
-
-    }
-
-    private static boolean esViable(int[][] mat) {
-        MinHeap<Integer, Integer> HeapCol = new MinHeap<>(mat[0].length);
-        for (int i = 0; i < mat.length; i++) {
-            MinHeap<Integer, Integer> HeapFila = new MinHeap<>(mat[0].length);
-            for (int j = 0; j < mat[0].length; j++) {
-                if (j == i) {
-                    HeapCol.push(mat[i][j], 1);
+    private static boolean sudokuBT(int[][] mat, int i, int j, int k) {
+        // if (esSol(mat, i, j, k)) {
+        //     return true;
+        // }
+        for (int p = 1; p <= k; p++) {
+            mat[i][j] = p;
+            if (esViable(mat, k)) {
+                for (int f = i; f < mat.length; f++) {
+                    for (int c = 0; c < mat[0].length; c++) {
+                        if (mat[f][c] == 0) {
+                            if (sudokuBT(mat, f, c, k)) {
+                                return true;
+                            }
+                        }
+                    }
                 }
-                HeapFila.push(mat[i][j], 1);
-            }
-            int aux = HeapFila.pop();
-            while (HeapFila.size() != 0) {
-                int aux2=HeapFila.pop();
-                if (aux2 == aux) {
-                    return false;
-                }
-                aux=aux2;
             }
         }
-        int aux = HeapCol.pop();
-        while (HeapCol.size() != 0) {
-            int aux2=HeapCol.pop();
-            if (aux2 == aux) {
-                return false;
+        return false;
+    }
+
+    private static boolean esViable(int[][] mat, int k) {
+        boolean[] usados = new boolean[k + 1];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (!usados[mat[i][j]]) {
+                    usados[mat[i][j]] = true;
+                } else {
+                    return false;
+                }
             }
-            aux=aux2;
+        }
+        for (int i = 0; i < usados.length; i++) {
+            usados[i] = false;
+        }
+        for (int i = 0; i < mat[0].length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (!usados[mat[j][i]]) {
+                    usados[mat[j][i]] = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < usados.length; i++) {
+            usados[i] = false;
+        }
+        int valFila = mat.length / 3;
+        int valCol = mat[0].length / 3;
+        for (int i = 0; i < mat.length; i += valFila) {
+            for (int j = 0; j < mat[0].length; j += valCol) {
+                int x = 0;
+                int y = 0;
+                while (x < valFila) {
+                    y=0;
+                    while (y < valCol) {
+                        if (!usados[mat[i + x][j + y]]) {
+                            usados[mat[i + x][j + y]] = true;
+                        } else {
+                            return false;
+                        }
+                        y++;
+                    }
+                    x++;
+                }
+
+            }
         }
         return true;
     }
+
 }
