490c917a633f9e66ef8a2c2b2407ba147bf39ed7
NicoPereiro <n.pereiro0@gmail.com>
2023-11-15 20:36:42 -0300

    Terminado Ejercicio10

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import tads.Pair;
+
 public class Ejercicio10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -13,39 +15,54 @@ public class Ejercicio10 {
                 mat[i][j] = Integer.parseInt(fila[j]);
             }
         }
+        in.close();
         int k = largo / 3 * ancho / 3;
         int[][] res = sudoku(mat, k);
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    public static int[][] sudoku(int[][]mat, int k){
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j] ==0){
-                    sudokuBT(mat, k, i, j);
-                    break;
-                }
-            }
+    public static int[][] sudoku(int[][] mat, int k) {
+        Pair<Integer, Integer> lugarDe0 = buscarCero(mat);
+        if (lugarDe0 == null) {
+            return mat;
         }
+        sudokuBT(mat, k, lugarDe0.fst, lugarDe0.snd);
         return mat;
     }
 
+    private static Pair<Integer, Integer> buscarCero(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
+                    Pair<Integer, Integer> lugar = new Pair<Integer, Integer>(i, j);
+                    return lugar;
+                }
+            }
+        }
+        return null;
+    } 
+
     private static boolean sudokuBT(int[][] mat, int k, int i, int j) {
-        if (esViable(mat, k)) {
+        if (buscarCero(mat) == null) {
             return true;
         }
         for (int p = 1; p <= k; p++) {
             mat[i][j] = p;
             if (esViable(mat, k)) {
-                for (int f = i; f < mat.length; f++) {
-                    for (int c = 0; c < mat[0].length; c++) {
-                        if (mat[f][c] == 0) {
-                            if (sudokuBT(mat, f, c, k)) {
-                                return true;
-                            }
-                        }
-                    }
+                Pair<Integer, Integer> lugarDe0 = buscarCero(mat);
+                if(lugarDe0 == null){
+                    return true;
+                }
+                if (sudokuBT(mat, k, lugarDe0.fst, lugarDe0.snd)) {
+                    return true;
                 }
             }
+            mat[i][j]=0;
         }
         return false;
     }
@@ -54,28 +71,33 @@ public class Ejercicio10 {
         boolean[] usados = new boolean[k + 1];
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                if (!usados[mat[i][j]] && mat[i][j] != 0) {
-                    usados[mat[i][j]] = true;
-                } else {
-                    return false;
+                if (mat[i][j] != 0) {
+                    if (!usados[mat[i][j]]) {
+                        usados[mat[i][j]] = true;
+                    } else {
+                        return false;
+                    }
                 }
             }
-        }
-        for (int i = 0; i < usados.length; i++) {
-            usados[i] = false;
-        }
-        for (int i = 0; i < mat[0].length; i++) {
-            for (int j = 0; j < mat.length; j++) {
-                if (!usados[mat[j][i]] && mat[i][j] != 0) {
-                    usados[mat[j][i]] = true;
-                } else {
-                    return false;
-                }
+            for (int m = 0; m < usados.length; m++) {
+                usados[m] = false;
             }
         }
-        for (int i = 0; i < usados.length; i++) {
-            usados[i] = false;
+        for (int j = 0; j < mat[0].length; j++) {
+            for (int i = 0; i < mat.length; i++) {
+                if (mat[i][j] != 0) {
+                    if (!usados[mat[i][j]]) {
+                        usados[mat[i][j]] = true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            for (int m = 0; m < usados.length; m++) {
+                usados[m] = false;
+            }
         }
+
         int valFila = mat.length / 3;
         int valCol = mat[0].length / 3;
         for (int i = 0; i < mat.length; i += valFila) {
@@ -85,16 +107,20 @@ public class Ejercicio10 {
                 while (x < valFila) {
                     y = 0;
                     while (y < valCol) {
-                        if (!usados[mat[i + x][j + y]] && mat[i + x][j + y] != 0) {
-                            usados[mat[i + x][j + y]] = true;
-                        } else {
-                            return false;
+                        if (mat[i + x][j + y] != 0) {
+                            if (!usados[mat[i + x][j + y]]) {
+                                usados[mat[i + x][j + y]] = true;
+                            } else {
+                                return false;
+                            }
                         }
                         y++;
                     }
                     x++;
                 }
-
+                for (int m = 0; m < usados.length; m++) {
+                    usados[m] = false;
+                }
             }
         }
         return true;
